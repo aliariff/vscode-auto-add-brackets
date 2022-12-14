656f99cab78582dcc6ce7c30b85935ee99c6cd0c
Ali Ariff <ali.ariff12@gmail.com>
2022-12-14 14:42:26 +0100

    chore: disable extension while testing

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,21 +1,35 @@
-import * as path from 'path';
+import * as path from "path";
 
-import { runTests } from '@vscode/test-electron';
+import { runTests } from "@vscode/test-electron";
 
 async function main() {
   try {
     // The folder containing the Extension Manifest package.json
     // Passed to `--extensionDevelopmentPath`
-    const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+    const extensionDevelopmentPath = path.resolve(__dirname, "../../");
 
     // The path to test runner
     // Passed to --extensionTestsPath
-    const extensionTestsPath = path.resolve(__dirname, './suite/index');
+    const extensionTestsPath = path.resolve(__dirname, "./suite/index");
 
     // Download VS Code, unzip it and run the integration test
-    await runTests({ extensionDevelopmentPath, extensionTestsPath });
+    await runTests({
+      extensionDevelopmentPath,
+      extensionTestsPath,
+      /**
+       * A list of launch arguments passed to VS Code executable, in addition to `--extensionDevelopmentPath`
+       * and `--extensionTestsPath` which are provided by `extensionDevelopmentPath` and `extensionTestsPath`
+       * options.
+       *
+       * If the first argument is a path to a file/folder/workspace, the launched VS Code instance
+       * will open it.
+       *
+       * See `code --help` for possible arguments.
+       */
+      launchArgs: ["--disable-extensions"],
+    });
   } catch (err) {
-    console.error('Failed to run tests');
+    console.error("Failed to run tests");
     process.exit(1);
   }
 }
